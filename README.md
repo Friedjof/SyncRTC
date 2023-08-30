8b05b4dd2e210692cd105aee2d40fef1acb3d811
Friedjof <friedjof@noweck.info>
2023-08-30 14:41:37 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ESP32 NTP RTC
+## Description
+This project synchorizes the ESP32 internal RTC with a NTP server. The RTC is used to keep track of time.
+With the onboard RTC it is possible to keep track of time even if the ESP32 is not connected to the internet.
+With the NTP server the RTC can be synchronized to the current time. For this we need to connect to the internet via WiFi.
+
+After flashing the ESP32 with the code, the ESP32 will wait for a serial connection. The ESP32 will then try to connect to the WiFi network. If the connection is successful, the ESP32 will initialize the RTC. In the main loop the ESP will log the current time of the external rtc and the internal rtc. Then the ESP32 will try to synchronize the external RTC with the internal RTC. After the synchronization the ESP32 will wait for one second and the loop will start again.
+
+## Hardware
+* ESP32 development board
+* USB cable
+* External RTC (Real Time Clock) module
+
+## Software
+* VSCode and PlatformIO
+* ESP32 Arduino Core
+* NTPClient library
+* RTClib library
+* WiFi library
+* SPI library
+
+## Configuration
+* Set the WiFi SSID and password in the `src/main.cpp` file.
+* Set the NPT server and time zone in the `src/main.cpp` file.
+
+**More information about the configuration can be found at the top of the `src/main.cpp` file. There you can also finde a description how to connect the external RTC module to the ESP32.**
---
+++ src/main.cpp
@@ -0,0 +1,134 @@
+/*
++-------------------------------------------------------------------------------------------+
+| ESP32 NTP RTC                                                                             |
+|                                                                                           |
+| This code is in the public domain.                                                        |
+| Written by Friedjof Noweck - 2023                                                         |
++-------------------------------------------------------------------------------------------+ 
+| With this code you can sync the time of an external RTC with the time of an NTP server.   |
+| Get ready to sync time with NTP server:                                                   |
+| 1. Change the SSID and password to match your WiFi network                                |
+| 2. You might need to change the NTP server to match your region                           |
+|   - Find your NTP server: https://www.pool.ntp.org/zone/                                  |
+|   - Replace "pool.ntp.org" with your NTP server                                           |
+|   - set your TZ example: "CET-1CEST,M3.5.0,M10.5.0/3" (Central European Time)             |
+|     - Find your TZ: https://remotemonitoringsystems.ca/time-zone-abbreviations.php        |
+|     - Replace "CET-1CEST,M3.5.0,M10.5.0/3" with your TZ                                   |
+| 3. Upload the sketch to your ESP32                                                        |   
+| 4. Open the serial monitor and wait for the ESP32 to connect to your WiFi network         |
+| 5. Wait for the ESP32 to sync time with the NTP server                                    |
++-------------------------------------------------------------------------------------------+ 
+| Connect the external RTC to your ESP32:                                                   |
+| - SDA RTC -> pin 21 ESP32                                                                 |
+| - SCL RTC -> pin 22 ESP32                                                                 |
+| - VCC RTC -> 3.3V ESP32                                                                   |
+| - GND RTC -> GND ESP32                                                                    |
++-------------------------------------------------------------------------------------------+ 
+| Sync time with NTP server:                                                                |
+| 1. Call syncTime() to sync time with NTP server                                           |
+| 2. Call logRTC() to print the time of the external RTC                                    |
+| 3. Call logNTP() to print the time of the NTP server                                      |
++-------------------------------------------------------------------------------------------+
+*/
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <SPI.h>
+#include "time.h"
+#include "RTClib.h"
+
+// Replace with your network credentials
+#define WIFI_SSID "<your wifi ssid>"         // <--- Change this
+#define WIFI_PASSWORD "<your wifi password>" // <--- Change this
+// Replace with your NTP server
+#define NTP_SERVER "pool.ntp.org"            // <--- Change this
+#define TZ "GMT-1"                           // <--- Change this
+// Offset in seconds to match your timezone
+#define GMT_OFFSET_SEC 0
+#define DAYLIGHT_OFFSET_SEC 3600
+#define SERIAL_BAUD 115200
+
+// Function prototypes
+void syncTime();
+void logRTC();
+void logNTP();
+
+// Init external RTC
+RTC_DS3231 rtc;
+
+void setup () {
+  // Init serial connection
+  Serial.begin(SERIAL_BAUD);
+  while (!Serial);
+
+  // connect to wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  // Sync time with NTP server
+  configTime(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+
+  // Set timezone
+  setenv("TZ", TZ, 1);
+
+  // Init external RTC
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+}
+
+void loop() {
+  // Print RTC and time from NTP server
+  logRTC();
+  // Print onboarding time
+  logNTP();
+
+  // Sync time with NTP server
+  syncTime();
+
+  Serial.println("---------------------------------------");
+
+  delay(1000); // Warte eine Sekunde, bevor du erneut synchronisierst
+}
+
+void syncTime() {
+  // Sync time with NTP server with RTC as time source
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  // Set RTC time
+  DateTime dt = DateTime(
+    timeinfo.tm_year + 1900,
+    timeinfo.tm_mon + 1,
+    timeinfo.tm_mday,
+    timeinfo.tm_hour,
+    timeinfo.tm_min,
+    timeinfo.tm_sec
+  );
+
+  rtc.adjust(dt);
+}
+
+void logRTC() {
+  // Print RTC time
+  Serial.print("External RTC: ");
+  time_t now = rtc.now().unixtime();
+  Serial.print(ctime(&now));
+}
+
+void logNTP() {
+  // Print time from NTP server
+  Serial.print("Internal RTC: ");
+  time_t now;
+  time(&now);
+  Serial.print(ctime(&now));
+}
